4e767c0ae59155fe214ec86ed706b6b920160df9
Ed Armstrong <ed@frar.ca>
2020-04-03 14:03:19 -0400

    checkpoint

--- jstest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-node src/test/js/TranslatorTest.js
-node src/test/js/GenerateJSON.js target/test-data/from-js.json
-node src/test/js/TestJavaToJS.js target/test-data/from-js.json
-node src/test/js/TestJavaToJS.js target/test-data/from-java.json
-node src/test/js/SocketTest.js
